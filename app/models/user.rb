eae046402012a8d8f3bcd58cc3a2631a2fe57b9c
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-10-26 18:24:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true
+  # データを登録する際に、一定の制約をかけること
+  # validates :カラム名, バリデーションの種類
+  # presence: trueでからの場合登録不可
 end
