f737c45392af4638eeb7ff802abbdbbf261108c2
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-10-26 17:57:51 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
 # railsの初期画面でトップページへ飛ぶように設定（P:コントローラー名とアクション名はダブルで囲む）
+  resources :users, only: [:edit, :update]
 end
