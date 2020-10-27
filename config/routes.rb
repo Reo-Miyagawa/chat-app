e59628216cefa014babe5e5bad75ff0ed735503f
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-10-27 14:14:03 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root to: "messages#index"
+  # get 'messages/index'
+  root to: "rooms#index"
 # railsの初期画面でトップページへ飛ぶように設定（P:コントローラー名とアクション名はダブルで囲む）
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
